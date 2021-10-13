f2d76defaaea1b2c81f019508cb204c51121a507
Денис <@DESKTOP-EKTHKC1>
2021-10-13 17:47:10 +0300

    Add RabbitMQService to WebApi

--- Application/ProductPriceStatistics.WebApi/Startup.cs
+++ Application/ProductPriceStatistics.WebApi/Startup.cs
@@ -5,11 +5,16 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using ProductPriceStatistics.Core.CommandHandlers;
+using ProductPriceStatistics.Core.Commands;
 using ProductPriceStatistics.Core.DTOs;
 using ProductPriceStatistics.Core.Queries;
 using ProductPriceStatistics.Core.QueryHandlers;
 using ProductPriceStatistics.Core.ReadRepositories;
+using ProductPriceStatistics.Core.Repositories;
 using ProductPriceStatistics.Infrastructure.EFCoreRepository;
+using ProductPriceStatistics.Infrastructure.RabbitMQService;
+using ProductPriceStatistics.WebApi.Services;
 using System.Collections.Generic;
 
 namespace ProductPriceStatistics.WebApi
@@ -46,11 +51,19 @@ namespace ProductPriceStatistics.WebApi
                 .UseNpgsql(dbContextConfiguration.ConnectionString)
                 .Options;
 
+            var rabbitMQServiceConfiguration = new RabbitMQServiceConfiguration();
+            Configuration.GetSection(RabbitMQServiceConfiguration.ConfigurationKey).Bind(rabbitMQServiceConfiguration);
+
+            services.AddSingleton(rabbitMQServiceConfiguration);
             services.AddSingleton<ProductPriceStatisticsDbContext>(new ProductPriceStatisticsDbContext(dbOptions));
             services.AddSingleton(typeof(IQueryHandler<GetPricesOfProductQuery, IEnumerable<PriceDto>>), typeof(GetPricesOfProductQueryHandler));
             services.AddSingleton(typeof(IQueryHandler<IEnumerable<ProductDto>>), typeof(GetAllProductsQueryHandler));
+            services.AddSingleton(typeof(ICommandHandler<AddPriceToProductCommand>), typeof(AddPriceToProductCommandHandler));
             services.AddSingleton(typeof(IPriceReadRepository), typeof(PriceReadRepository));
             services.AddSingleton(typeof(IProductReadRepository), typeof(ProductReadRepository));
+            services.AddSingleton(typeof(IProductRepository), typeof(ProductRepository));
+            services.AddSingleton(typeof(IPriceRepository), typeof(PriceRepository));
+            services.AddHostedService<AddPriceToProductHandlerHostedService>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
